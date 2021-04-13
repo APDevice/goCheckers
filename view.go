c80c6f69a5785420431018d01609156484f08913
APDevice <59993100+APDevice@users.noreply.github.com>
2021-04-13 03:07:09 -0700

    restored missing layout function

--- view.go
+++ view.go
@@ -2,6 +2,12 @@ package main
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
